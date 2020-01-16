b6109951bb64a206720d0728139ef366bee982fe
Nikhil Singhal <niks@mit.edu>
2020-01-15 22:30:56 -0500

    remove autocomplete

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -39,7 +39,7 @@ class App extends Component {
         <label className="App-label" htmlFor="dog-breed">
           enter dog breed:{" "}
         </label>
-        <input onKeyDown={this.onKeyDown} id="dog-breed" />
+        <input onKeyDown={this.onKeyDown} id="dog-breed" autocomplete="off" />
         <GoodBoiMeter breed={breed} />
         <DogViewer breed={breed} iteration={iteration} />
       </div>
