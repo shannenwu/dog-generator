fc7d28725f519bedb7ba81db1af41b551c5d6a39
Nikhil Singhal <niks@mit.edu>
2020-01-16 02:19:58 -0500

    make controlled component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,15 +9,22 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      input: "",
       breed: "pembroke",
       iteration: 0
     };
   }
 
+  handleChange = event => {
+    this.setState({
+      input: event.target.value
+    });
+  };
+
   onKeyDown = event => {
     if (event.keyCode === 13) {
       // 13 is the enter key
-      if (event.target.value === "") {
+      if (this.state.input === "") {
         // refresh the current dog if you just hit enter
         this.setState({
           iteration: this.state.iteration + 1
@@ -25,22 +32,28 @@ class App extends Component {
       } else {
         // load a new dog if you typed a breed
         this.setState({
-          breed: event.target.value,
+          input: "",
+          breed: this.state.input,
           iteration: this.state.iteration + 1
         });
-        event.target.value = "";
       }
     }
   };
 
   render() {
-    const { breed, iteration } = this.state;
+    const { input, breed, iteration } = this.state;
     return (
       <div className="u-flex u-flexColumn u-flex-alignCenter">
         <label className="App-label" htmlFor="dog-breed">
           enter dog breed:{" "}
         </label>
-        <input onKeyDown={this.onKeyDown} id="dog-breed" autoComplete="off" />
+        <input
+          value={input}
+          onChange={this.handleChange}
+          onKeyDown={this.onKeyDown}
+          id="dog-breed"
+          autoComplete="off"
+        />
         <GoodBoiMeter breed={breed} />
         <DogViewer breed={breed} iteration={iteration} />
       </div>
